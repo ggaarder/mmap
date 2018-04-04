fbdcddbc3e73cf951b5ca1435d846b91d5b77746
George Gaarder <ggaarder@foxmail.com>
2018-04-04 22:48:04 +0800

    ok

---
+++ mmap.c
@@ -0,0 +1,17 @@
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <tcclib.h>
+
+int main() {
+  int fd = open("mmap.c", O_RDONLY);
+  int len = lseek(fd, 0, SEEK_END)+1;
+  lseek(fd, 0, SEEK_SET);
+  printf("len: %d\n", len);
+  char *txt = (char*)mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
+  char *p;
+  for (p = txt; p != txt+len; ++p) putchar(*p);
+  munmap(txt, len);
+  close(fd);
+}
