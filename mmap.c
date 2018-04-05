3273e2ec47d484ff95fff69bc46dd0a9ae6b39b3
George Gaarder <ggaarder@foxmail.com>
2018-04-05 11:14:35 +0800

    test symbolic link

--- mmap.c
+++ mmap.c
@@ -4,14 +4,19 @@
 #include <unistd.h>
 #include <tcclib.h>
 
-int main() {
-  int fd = open("mmap.c", O_RDONLY);
+void test(char *fn) {
+  int fd = open(fn, O_RDONLY);
   int len = lseek(fd, 0, SEEK_END)+1;
   //lseek(fd, 0, SEEK_SET);
-  printf("len: %d\n", len);
+  printf("%s len: %d\n", fn, len);
   char *txt = (char*)mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
   char *p;
   for (p = txt; p != txt+len; ++p) putchar(*p);
   munmap(txt, len);
   close(fd);
+}
+
+int main() {
+  test("mmap.c");
+  test("train-labels.idx1-ubyte");
 }
